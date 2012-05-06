47239a363693d562a9a295415bfac43f5ec90adf
Michaël Rigart <michael@netronix.be>
2012-05-06 13:17:29 +0200

    add memory info

--- lib/sys_viewer/osx.rb
+++ lib/sys_viewer/osx.rb
@@ -1,5 +1,33 @@
 module SysViewer
   module Osx
 
+    def memory_info
+      memory = { memtotal: 0, memused: 0, memfree: 0, swaptotal: 0, swapused: 0, swapfree: 0 }
+
+      stdin, stdout, stderr = Open3.popen3('sysctl', '-n', 'hw.memsize')
+      memory[:memtotal] = stdout.gets.to_f / 1048576
+
+      stdin, stdout, stderr = Open3.popen3('vm_stat')
+      vm_lines = stdout.readlines
+      vm_lines.pop # remove footer
+      vm_lines.shift # remove header
+
+      vm_stats = {}
+      vm_lines.each do |line|
+        element = line.split(/:[\s]+/)
+        vm_stats[element[0]] = element[1].gsub!(/.[\n]+/, '').to_f * 4096
+      end
+      memory[:memfree] = vm_stats['Pages free'] / 1048576 + vm_stats['Pages speculative'] / 1048576
+      memory[:memused] = memory[:memtotal] - memory[:memfree]
+
+      stdin, stdout, stderr = Open3.popen3('sysctl', '-n', 'vm.swapusage')
+      swap_info = stdout.gets.split(/[\s]+/)
+
+      memory[:swaptotal] = swap_info[2].gsub('M', '').to_f
+      memory[:swapused] = swap_info[5].gsub('M', '').to_f
+      memory[:swapfree] = swap_info[8].gsub('M', '').to_f
+
+      memory
+    end
   end
 end
--- test/test_sys_viewer.rb
+++ test/test_sys_viewer.rb
@@ -16,4 +16,24 @@ class TestSysViewer < Test::Unit::TestCase
   test "user" do
     assert_equal "michael", SysViewer.user
   end
+
+  test "memory_info" do
+    assert_respond_to SysViewer, :memory_info
+  end
+
+  test "memory info" do
+    response = SysViewer.memory_info
+
+    assert response.kind_of?(Hash)
+
+    assert response.has_key?(:memtotal)
+    assert response.has_key?(:memused)
+    assert response.has_key?(:memfree)
+    assert response.has_key?(:swaptotal)
+    assert response.has_key?(:swapused)
+    assert response.has_key?(:swapfree)
+
+    response.each { |key, value| assert value.kind_of?(Float) }
+
+  end
 end
